2264995e77b67a485f456aacbf67c5716305c8a5
gabrielrsh <gabriel.shiota@gmail.com>
2023-08-12 03:30:18 -0300

    fix: limpando class App

--- src/main/App.java
+++ src/main/App.java
@@ -1,18 +1,8 @@
 package main;
 
-import main.list.Pesquisa.exercicio1.CatalogoLivros;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        CatalogoLivros catalogoLivros = new CatalogoLivros();
-
-        catalogoLivros.adicionarLivro("Livro de 1", "kleiton", 2000);
-        catalogoLivros.adicionarLivro("Livro de 1", "craudio", 2022);
-        catalogoLivros.adicionarLivro("Livro de 2", "kleiton", 1888);
-        catalogoLivros.adicionarLivro("Livro de 3", "junin", 1990);
-
-        //System.out.println(catalogoLivros.pesquisarPorAutor("kleiton"));
-        //System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2030, 2050));
-        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro de 1"));
+        
     }
 }
